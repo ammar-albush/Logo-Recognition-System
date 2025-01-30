7b528d63506887f379fb787f778ad99d4a0f216e
Ammar <41263980+ammar-albush@users.noreply.github.com>
2025-01-30 19:24:50 +0100

    v1

---
+++ LogoRecognitionSystem/BLob.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogoRecognitionSystem
+{
+    public class BLob
+    {
+
+        public Bitmap Image { get; set; }
+
+        public Rectangle Location { get; set; }
+
+        public BLob(Bitmap bitmap,Rectangle rectangle)
+        {
+            this.Image = bitmap;
+            this.Location = rectangle;
+
+        }
+
+    }
+}
